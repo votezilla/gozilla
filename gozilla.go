8780a4ede00b1bdbc8f1eb9153aa6b16bced37ba
Aaron Smith <magicsquare15@gmail.com>
2017-06-18 23:14:13 +0700

    Refactoring login form to be auto-generated

--- gozilla.go
+++ gozilla.go
@@ -3,20 +3,48 @@
 package main
 
 import (
+    "bytes"
+    "github.com/bluele/gforms"
     "html/template"
     "log"
-    "net/http"
+    "net/http"    
 )
 
-type Page struct {
-    Title string
-    Body  []byte
+type LoginForm struct {
+    Username string  `gforms:"username"`
+    Password float32 `gforms:"password"`
 }
 
 var (
-    templates *template.Template = nil
-        debug = true
+    userForm gforms.Form
+
+    tplText = `
+<form method="post">
+  {{range $i, $field := .Fields}}
+    <label>{{$field.GetName}}: </label>{{$field.Html | safeHTML}}
+      {{range $ei, $err := $field.Errors}}
+        <label class="error">{{$err}}</label>
+      {{end}}
+    <br />
+  {{end}}<input type="submit">
+</form>`
 )
+
+var (
+    templates *template.Template = nil
+    debug = true
+)
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// utility functions
+//
+///////////////////////////////////////////////////////////////////////////////
+func check(err error) {
+    if err != nil {
+        log.Fatal(err)
+    }
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -25,10 +53,14 @@ var (
 ///////////////////////////////////////////////////////////////////////////////
 func parseTemplateFiles() {
     var err error
-    templates, err = template.ParseFiles("templates/frontPage.html",
-                                         "templates/forgotPassword.html",
-                                         "templates/login.html",
-                                         "templates/register.html")
+    
+    t := template.New("").Funcs(template.FuncMap { "safeHTML": func(x string) interface{} { log.Printf("safeHTML\n"); return template.HTML(x) } })
+
+    templates, err = t.ParseFiles("templates/frontPage.html",
+                                  "templates/forgotPassword.html",
+                                  "templates/login.html",
+                                  "templates/register.html")
+
     if err != nil {
         log.Fatal(err)
     }
@@ -64,8 +96,55 @@ func frontPageHandler(w http.ResponseWriter, r *http.Request) {
 //
 ///////////////////////////////////////////////////////////////////////////////
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-    var args struct{}
-    renderTemplate(w, "login", &args)
+    var args struct{
+        //formHTML bytes.Buffer
+        FormHTML string
+    }
+    //renderTemplate(w, "login", &args)
+    
+    userForm = gforms.DefineForm(gforms.NewFields(
+        gforms.NewTextField(
+            "name",
+            gforms.Validators{
+                gforms.Required(),
+                gforms.MaxLengthValidator(32),
+            },
+        ),
+        gforms.NewFloatField(
+            "weight",
+            gforms.Validators{},
+        ),
+    ))
+
+    //var err error
+    tpl := template.New("tpl").Funcs(
+            template.FuncMap { 
+                "safeHTML": func(x string) interface{} { log.Printf("safeHTML\n"); return template.HTML(x) }},)
+    tpl, _ = tpl.Parse(tplText)
+
+    //tpl := template.Must(template.New("tpl").Parse(tplText))
+    log.Printf("tpl: %v\n", tpl)
+   // log.Printf("tpl: %s\n", string(tpl))
+    
+    form := userForm(r)
+    
+    if r.Method != "POST" {
+        //tpl.Execute(w, form)
+        //return
+        
+        var formHTML bytes.Buffer
+        
+        check(tpl.Execute(&formHTML, form))
+        
+        args.FormHTML = formHTML.String()
+        
+        log.Printf("processed form buffer: %s\n", args.FormHTML)
+    }
+    
+    log.Printf("form: %v\n", form)
+    
+    //renderTemplate(w, "login", &form)
+    renderTemplate(w, "login", args)
 }
 
 func postLoginHandler(w http.ResponseWriter, r *http.Request) {
